2656d9913db550d4e5ddbb30973b437c58ede5ae
Woody Gelius <gelius24@hotmail.com>
2021-08-29 18:47:17 -0400

    v2

--- portfolioV1/scripts/modals.js
+++ portfolioV1/scripts/modals.js
@@ -4,6 +4,9 @@ const closeButtons = document.getElementsByClassName("modalCloseButton");
 const open = [];
 const containers = [];
 const close = [];
+let showCompleteModal = true
+const modalsP = document.getElementsByClassName('modalP')
+const descs = []
 
 function set_ids() {
   for (let i = 0; i < openButtons.length; i++) {
@@ -17,6 +20,10 @@ function set_ids() {
   for (let i = 0; i < closeButtons.length; i++) {
     closeButtons[i].setAttribute("id", `closeBtn${i + 1}`);
     close.push(document.getElementById(`closeBtn${i + 1}`));
+  }
+  for (let i = 0; i < modalsP.length; i++) {
+    modalsP[i].setAttribute("id", `desc${i + 1}`);
+    descs.push(document.getElementById(`desc${i + 1}`));
   }
 }
 
@@ -32,7 +39,16 @@ function listen() {
       containers[closeBtn.id.charAt(closeBtn.id.length - 1) -1].classList.remove("show");
     });
   });
-}
 
+  descs.map((desc) => {
+    addEventListener("resize", () => {
+      if(screen.width < 1116){
+        descs[desc.id.charAt(desc.id.length - 1) -1].classList.add("show_p")
+      } else {
+        descs[desc.id.charAt(desc.id.length - 1) -1].classList.remove("show_p")
+      }
+    });
+  });
+}
 set_ids();
 listen()
